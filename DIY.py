e4cd4d4d6b021004e6d072f16eb82fc8a83e7fc8
coco177938 <162773917+coco177938@users.noreply.github.com>
2024-08-01 18:14:30 +0800

    Update DIY.py

--- DIY.py
+++ DIY.py
@@ -1,1 +1,26 @@
-自定义脚本
+import requests
+
+API_TOKEN = "<API TOKEN>"
+ZONE_ID = "<ZONE ID>"
+
+baseUrl = f"https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records"
+
+headers = {
+    'Authorization': f'Bearer {API_TOKEN}',
+    'Content-Type': 'application/json'
+}
+
+listUrl = f"{baseUrl}?per_page=500"
+print(listUrl)
+response = requests.get(listUrl, headers=headers)
+records = response.json()['result']
+
+for record in records:
+    name = record['name']
+    content = record['content']
+
+    print(f"Deleting {name} that points to {content}")
+
+    deleteUrl = f"{baseUrl}/{record['id']}"
+    requests.delete(deleteUrl, headers=headers)
+    print(deleteUrl)
